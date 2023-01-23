5b04cd364f30b79afb849d3a98313b668e71e887
NicolasBriE <briennonnicolas@gmail.com>
2023-01-23 17:09:42 +0100

    recuperation articles 9

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,4 @@
-const produits = fetch('http://localhost:3000/Product.js').then(produits => produits.json());
+const produits = await fetch('http://localhost:3000/Product.js').then(produits => produits.json());
 
 
 function genererProduits(produits) {
