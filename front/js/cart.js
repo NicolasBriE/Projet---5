babf3c92b1434b19e1323ebda7ad2b62bd52564f
NicolasBriE <briennonnicolas@gmail.com>
2023-03-22 15:17:50 +0100

    Passer la commande

--- front/js/cart.js
+++ front/js/cart.js
@@ -157,4 +157,154 @@ fetch('http://localhost:3000/api/Products/')
 
 
 
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let address = document.getElementById("address");
+let city = document.getElementById("city");
+let email = document.getElementById("email");
 
+let nameRegex = /^[A-Z][A-Za-z\é\è\ê\-]+$/;
+let addressRegex = /^[a-zA-Z0-9\s,'-]*$/;
+let emailRegex = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/;
+
+let verifFirstName;
+let verifLastName;
+let verifAddress;
+let verifCity;
+let verifEmail;
+
+firstName.addEventListener("change", function () {
+    let firstNameErrorMessage = document.getElementById("firstNameErrorMsg");
+
+
+
+    if (nameRegex.test(firstName.value)) {
+        verifFirstName = true;
+        console.log(verifFirstName);
+        firstNameErrorMessage.innerText = "";
+    }
+
+    else {
+        verifFirstName = false;
+        console.log(verifFirstName);
+        firstNameErrorMessage.innerText = "Veuillez remplir le champ correctement.";
+    }
+
+})
+
+lastName.addEventListener("change", function () {
+    let lastNameErrorMessage = document.getElementById("lastNameErrorMsg");
+
+
+
+    if (nameRegex.test(lastName.value)) {
+        verifLastName = true;
+        console.log(verifLastName);
+        lastNameErrorMessage.innerText = "";
+    }
+
+    else {
+        verifLastName = false;
+        console.log(verifLastName);
+        lastNameErrorMessage.innerText = "Veuillez remplir le champ correctement.";
+    }
+})
+
+address.addEventListener("change", function () {
+    let addressErrorMessage = document.getElementById("addressErrorMsg");
+
+
+
+    if (addressRegex.test(address.value)) {
+        verifAddress = true;
+        console.log(verifAddress);
+        addressErrorMessage.innerText = "";
+    }
+
+    else {
+        verifAddress = false;
+        console.log(verifAddress);
+        addressErrorMessage.innerText = "Veuillez remplir le champ correctement.";
+    }
+})
+
+city.addEventListener("change", function () {
+    let cityErrorMessage = document.getElementById("cityErrorMsg");
+
+
+
+    if (addressRegex.test(city.value)) {
+        verifCity = true;
+        console.log(verifCity);
+        cityErrorMessage.innerText = "";
+    }
+
+    else {
+        verifCity = false;
+        console.log(verifCity);
+        cityErrorMessage.innerText = "Veuillez remplir le champ correctement.";
+    }
+})
+
+email.addEventListener("change", function () {
+    let emailErrorMessage = document.getElementById("emailErrorMsg");
+
+
+
+    if (emailRegex.test(email.value)) {
+        verifEmail = true;
+        console.log(verifEmail);
+        emailErrorMessage.innerText = "";
+
+    }
+
+    else {
+        verifEmail = false;
+        console.log(verifEmail);
+        emailErrorMessage.innerText = "Veuillez remplir le champ correctement.";
+    }
+})
+
+boutonCommander = document.getElementById("order");
+
+
+boutonCommander.addEventListener("click", function () {
+    if (verifFirstName && verifLastName && verifAddress && verifCity && verifEmail) {
+
+        let idList = []
+
+        for (let i = 0; i < panier.length; i++) {
+            idList.push(panier[i].id)
+        }
+        console.log(idList)
+
+        let commande = {
+            contact: {
+                firstName: firstName.value,
+                lastName: lastName.value,
+                address: address.value,
+                city: city.value,
+                email: email.value,
+            },
+            products: idList,
+        }
+
+        let envoiCommande = {
+            method: 'POST',
+            body: JSON.stringify(commande),
+            headers: { 'Content-Type': 'application/json' }
+        }
+        console.log(envoiCommande);
+
+        fetch(`http://localhost:3000/api/Products/order`, envoiCommande)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                window.location.assign(`./confirmation.html?orderId=${data.orderId}`)
+
+            })
+
+    }
+})
